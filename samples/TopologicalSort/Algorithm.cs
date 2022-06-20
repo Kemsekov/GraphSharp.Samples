79ae16dfb4d2059f7727b71b6d54982f296cd6da
Vlad <pikova98@bk.ru>
2022-06-20 20:43:35 +0500

    Added topological sort sample

---
+++ samples/TopologicalSort/Algorithm.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphSharp.GraphStructures;
+using GraphSharp.Propagators;
+using GraphSharp.Visitors;
+public class Algorithm : Visitor<NodeXY, NodeConnector>
+{
+    public override PropagatorBase<NodeXY, NodeConnector> Propagator { get; }
+    public IList<IList<NodeXY>> Layers { get; }
+    public bool Done { get; private set; } = false;
+    public const byte Added = 4;
+    public Algorithm(GraphStructureBase<NodeXY, NodeConnector> graph)
+    {
+        Propagator = new ParallelPropagator<NodeXY, NodeConnector>(this, graph);
+        var incomingEdges = graph.CountIncomingEdges();
+        var startingNodes = new List<int>();
+        foreach (var count in incomingEdges)
+        {
+            if (count.Value == 0)
+            {
+                startingNodes.Add(count.Key);
+            }
+        }
+        Layers = new List<IList<NodeXY>>();
+        Propagator.SetPosition(startingNodes.ToArray());
+        this.EndVisit();
+    }
+
+    public override void EndVisit()
+    {
+        if (Done) return;
+        if (Layers.Count > 0)
+        {
+            if (Layers[^1].Count == 0)
+            {
+                this.Done = true;
+                return;
+            }
+        }
+        Layers.Add(new List<NodeXY>());
+    }
+
+    public override bool Select(NodeConnector edge)
+    {
+        if (Done) return false;
+        if (Propagator.IsNodeInState(edge.Target.Id, Added))
+        {
+            return false;
+        }
+        Propagator.SetNodeState(edge.Target.Id, Added);
+        return true;
+    }
+
+    public override void Visit(NodeXY node)
+    {
+        if (Done) return;
+        lock (Layers)
+            Layers[^1].Add(node);
+    }
+
+    public void DoTopologicalSort()
+    {
+        if (!Done) return;
+
+        var nodePositionShift = 1.0f / (Layers.Count() - 2);
+        var nodePosition = 0.01f;
+
+        foreach (var layer in Layers)
+        {
+            foreach (var node in layer)
+            {
+                node.Position = new(nodePosition, node.Position.Y);
+            }
+            nodePosition += nodePositionShift;
+        }
+    }
+}
---
+++ samples/TopologicalSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+ArgumentsHandler argz = new("settings.json");
+
+var graph = Helpers.CreateGraph(argz);
+graph.Do.CreateSources(65,77,39,40);
+
+Helpers.MeasureTime(()=>{
+    System.Console.WriteLine("Doing topological sort...");
+    var alg = new Algorithm(graph);
+    while(!alg.Done){
+        alg.Propagate();
+    }
+    alg.DoTopologicalSort();
+});
+Helpers.ShiftNodesToFitInTheImage(graph.Nodes);
+Helpers.CreateImage(argz,graph.Configuration,drawer=>{
+    drawer.Clear(Color.Black);
+    drawer.DrawEdgesParallel(graph.Edges,argz.thickness);
+    drawer.DrawDirections(graph.Edges,argz.thickness,argz.directionLength,Color.CadetBlue);
+    drawer.DrawNodesParallel(graph.Nodes,argz.nodeSize);
+    drawer.DrawNodeIds(graph.Nodes,Color.Azure,argz.fontSize);
+});
+System.Console.WriteLine("Done");
