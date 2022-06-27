4ee07a2c82307775069749e94b3c277dc6db97a0
Vlad <pikova98@bk.ru>
2022-06-27 11:35:52 +0500

    Updated Graph coloring algorithm to use implementation from library

--- samples/GraphColoring/Program.cs
+++ samples/GraphColoring/Program.cs
@@ -4,35 +4,20 @@ using System.Drawing;
 ArgumentsHandler argz = new("settings.json");
 
 var graph = Helpers.CreateGraph(argz);
-graph.Do.DelaunayTriangulation();
-graph.Do.MakeUndirected();
-// Helpers.SaveGraph(nodes,"graph.json");
 
-// var graph = Helpers.LoadGraph("graph.json");
+var usedColors = graph.Do.ColorNodes(
+    new[]{Color.Azure,Color.Yellow,Color.Red,Color.Coral,Color.Blue,Color.Aqua,Color.Violet},
+    x=>x.OrderBy(x=>graph.Edges[x.Id].Count())
+);
 
-var coloring = new Algorithm<NodeXY,NodeConnector>(graph,new[]{Color.Azure,Color.Yellow,Color.Red,Color.Coral,Color.Blue,Color.Aqua,Color.Violet});
 
-Helpers.MeasureTime(()=>{
-    System.Console.WriteLine("Starting coloring graph...");
-    for(int i = 0;i<argz.steps;i++){
-        if(coloring.Done()){
-            System.Console.WriteLine($"Coloring done at {i} step");
-            return;
-        }
-        coloring.Propagate();
-    }
-    System.Console.WriteLine("Done all steps");
-});
-
-graph.EnsureRightColoring();
-
-System.Console.WriteLine($"Total colors used : {coloring.UsedColors.Where(x=>x.Value!=0).Count()}");
-foreach(var colorInfo in coloring.UsedColors.OrderByDescending(x=>x.Value)){
+System.Console.WriteLine($"Total colors used : {usedColors.Where(x=>x.Value!=0).Count()}");
+foreach(var colorInfo in usedColors.OrderByDescending(x=>x.Value)){
     System.Console.WriteLine(colorInfo);
 }
+System.Console.WriteLine("Nodes colored : {0}", usedColors.Sum(x=>x.Value));
 
-System.Console.WriteLine("Nodes colored : {0}", coloring.UsedColors.Sum(x=>x.Value));
-
+graph.EnsureRightColoring();
 Helpers.CreateImage(argz,graph.Configuration,drawer=>{
     drawer.Clear(Color.Black);
     drawer.DrawEdges(graph.Edges,argz.thickness);
