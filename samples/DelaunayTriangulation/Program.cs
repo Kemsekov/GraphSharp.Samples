60f97374ff01228e5624bd727d1664f0c84b07b5
Vlad <pikova98@bk.ru>
2024-01-25 21:58:54 +0500

    update to sample

--- samples/DelaunayTriangulation/Program.cs
+++ samples/DelaunayTriangulation/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System.Drawing;
+using GraphSharp;
 
 ArgumentsHandler argz = new("settings.json");
 
 var graph = Helpers.CreateGraph(argz);
-graph.Do.DelaunayTriangulationWithoutHull(x=>x.Position);
-Helpers.ShiftNodesToFitInTheImage(graph.Nodes,x=>x.Position,(n,p)=>n.Position = p);
+graph.Do.DelaunayTriangulation(x=>x.MapProperties().Position);
+Helpers.ShiftNodesToFitInTheImage(graph.Nodes,x=>x.MapProperties().Position,(n,p)=>n.MapProperties().Position = p);
 Helpers.CreateImage(argz,graph,drawer=>{
     drawer.Clear(Color.Black);
     drawer.DrawEdgesParallel(graph.Edges,argz.thickness);
     drawer.DrawDirections(graph.Edges,argz.thickness,argz.directionLength,Color.CadetBlue);
     drawer.DrawNodesParallel(graph.Nodes,argz.nodeSize);
     // drawer.DrawNodeIds(graph.Nodes,Color.Azure,argz.fontSize);
-},x=>x.Position);
+},x=>x.MapProperties().Position);
