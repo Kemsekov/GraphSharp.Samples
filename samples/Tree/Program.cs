13314f6f798a61ec9a15ab868258b7a676d54661
Vlad <pikova98@bk.ru>
2024-01-25 21:49:52 +0500

    min spanning tree update

--- samples/Tree/Program.cs
+++ samples/Tree/Program.cs
@@ -6,8 +6,8 @@ ArgumentsHandler argz = new("settings.json");
 
 var graph = Helpers.CreateGraph(argz);
 IEnumerable<Edge> tree = new List<Edge>();
-var distance = (Node n1,Node n2)=>(double)(n1.Position-n2.Position).Length();
-graph.Do.DelaunayTriangulation(x=>x.Position);
+var distance = (Node n1,Node n2)=>(double)(n1.MapProperties().Position-n2.MapProperties().Position).L2Norm();
+graph.Do.DelaunayTriangulation(x=>x.MapProperties().Position);
 Helpers.MeasureTime(() =>
 {
     System.Console.WriteLine("Finding minimal spanning tree...");
@@ -19,11 +19,11 @@ foreach (var edge in tree)
 {
     edge.Color = Color.Azure;
 }
-Helpers.ShiftNodesToFitInTheImage(graph.Nodes,x=>x.Position,(n,p)=>n.Position = p);
+Helpers.ShiftNodesToFitInTheImage(graph.Nodes,x=>x.MapProperties().Position,(n,p)=>n.MapProperties().Position = p);
 Helpers.CreateImage(argz, graph, drawer =>
 {
     drawer.Clear(Color.Black);
     drawer.DrawEdgesParallel(graph.Edges, argz.thickness);
     // drawer.DrawNodesParallel(graph.Nodes, argz.nodeSize);
     drawer.DrawEdgesParallel(tree, argz.thickness);
-},x=>x.Position);
+},x=>x.MapProperties().Position);
