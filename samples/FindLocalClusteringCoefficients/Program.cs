53ea9c810e0856f681b58b81834a13c827b5269f
Vlad <pikova98@bk.ru>
2024-01-25 23:02:09 +0500

    local clustering coef update

--- samples/FindLocalClusteringCoefficients/Program.cs
+++ samples/FindLocalClusteringCoefficients/Program.cs
@@ -1,37 +1,33 @@
-﻿using System.Collections.Concurrent;
-using System.Diagnostics;
-using System.Drawing;
+﻿using System.Drawing;
 using GraphSharp;
-using GraphSharp.Common;
-using GraphSharp.Graphs;
-using GraphSharp.Propagators;
-using GraphSharp.Visitors;
 using MathNet.Numerics.LinearAlgebra.Single;
-using SampleBase;
+
+//bigger nodes will have bigger local clustering coefficient
 
 ArgumentsHandler argz = new("settings.json");
 var graph = Helpers.CreateGraph(argz);
-graph.Do.DelaunayTriangulation(x=>x.Position);
+graph.Do.DelaunayTriangulation(x=>x.MapProperties().Position);
 graph.Do.MakeBidirected();
 var coeffs = new double[0];
 Helpers.MeasureTime(() =>
 {
     System.Console.WriteLine("Finding local clustering coefficients");
-    coeffs = graph.Do.FindLocalClusteringCoefficients();
+    using var l_coeffs = graph.Do.FindLocalClusteringCoefficients();
+    coeffs=l_coeffs.ToArray();
 });
+
 coeffs.Select((value, index) =>
 {
     var node = graph.Nodes[index];
-    node.Weight = value;
+    node.MapProperties().Weight = value;
     return 1;
 }).ToArray();
 
-Helpers.ShiftNodesToFitInTheImage(graph.Nodes,x=>x.Position,(n,p)=>n.Position = p);
 Helpers.CreateImage(argz, graph, drawer =>
 {
     drawer.Clear(Color.Black);
     drawer.DrawEdgesParallel(graph.Edges, argz.thickness);
     foreach(var n in graph.Nodes)
-        drawer.DrawNode(n, argz.nodeSize*n.Weight);
+        drawer.DrawNode(n, argz.nodeSize*n.MapProperties().Weight);
     drawer.DrawNodeIds(graph.Nodes, Color.Wheat, argz.fontSize);
-},x=>x.Position);
+},x=> (Vector)(x.MapProperties().Position*0.9f+0.05f));
