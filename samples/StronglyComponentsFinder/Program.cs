896f969a0ecc8cff9f343fc410f924aa493bc675
Vlad <pikova98@bk.ru>
2024-01-25 23:04:22 +0500

    scc update

--- samples/StronglyComponentsFinder/Program.cs
+++ samples/StronglyComponentsFinder/Program.cs
@@ -1,20 +1,13 @@
-﻿using System.Diagnostics;
-using System.Drawing;
-using GraphSharp.Graphs;
-using GraphSharp.Propagators;
-using GraphSharp.Visitors;
-using MathNet.Numerics.LinearAlgebra.Single;
-using SampleBase;
+﻿using System.Drawing;
+using GraphSharp;
 
 ArgumentsHandler argz = new("settings.json");
 var graph = Helpers.CreateGraph(argz);
-var components = graph.Do.FindStronglyConnectedComponentsTarjan();
-System.Console.WriteLine(components.Sum(x=>x.nodes.Count()));
-System.Console.WriteLine(components.Count());
-foreach(var c in components){
+var scc = graph.Do.FindStronglyConnectedComponentsTarjan();
+foreach(var c in scc.Components){
     var color = Color.FromArgb(Random.Shared.Next(256),Random.Shared.Next(256),Random.Shared.Next(256));
     foreach(var n in c.nodes){
-        n.Color = color;
+        n.MapProperties().Color = color;
     }
 }
 
@@ -24,4 +17,4 @@ Helpers.CreateImage(argz, graph, drawer =>
     drawer.DrawEdgesParallel(graph.Edges, argz.thickness);
     drawer.DrawDirectionsParallel(graph.Edges,argz.thickness,argz.directionLength,Color.Orange);
     drawer.DrawNodesParallel(graph.Nodes, argz.nodeSize);
-},x=>x.Position);
+},x=>x.MapProperties().Position);
